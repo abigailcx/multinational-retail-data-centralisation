7cd8a9f0e5b03df541b001e4f2efb280dd49c733
Abigail Cox <abigailgcox@outlook.com>
2023-02-07 22:16:01 +0000

    [DRAFT] Adds product cleaning and extraction methods

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,10 @@
 # from database_utils import DatabaseConnector
+import boto3
+s3 = boto3.client('s3')
 import pandas as pd
+import requests
 import tabula
+from tqdm import tqdm
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
@@ -11,7 +15,8 @@ class DataExtractor:
     sources to extract from: CSV, API, S3 bucket
     """
     def __init__(self) -> None:
-        pass
+        self.header_dict = {'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'}
+        self.stores_dict = {}
 
     @staticmethod
     def read_rds_table(engine, table_name):
@@ -42,11 +47,60 @@ class DataExtractor:
         https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf
         """
         card_df = pd.concat(tabula.read_pdf(url, pages='all'))
-        print(card_df.head(10))
+        # print(card_df.head(10))
         # print(card_df)
         return card_df
+
+
+    def list_number_of_stores(self, number_of_stores_endpoint):
+        """
+        returns: number of stores to extract (int)
+        """
+        response = requests.get(number_of_stores_endpoint, headers=self.header_dict)
+        number_of_stores = response.json()['number_stores']
+        # print(type(number_of_stores))
+        return number_of_stores
+
+    
+    def retrieve_stores_data(self, retrieve_a_store_endpoint, number_of_stores_endpoint):
+        """
+        extracts all the stores from the API,
+        saving them in a Pandas dataframe
+        """
+        for store_number in tqdm(range(self.list_number_of_stores(number_of_stores_endpoint)), desc='Retrieving store data...'):
+            response = requests.get(f'{retrieve_a_store_endpoint}/{store_number}', headers=self.header_dict)
+            stores_data = response.json()
+            
+            self.stores_dict[store_number] = (stores_data)
+            # print(self.stores_dict)
+        stores_df_initialised = pd.DataFrame(self.stores_dict)
+        stores_df = stores_df_initialised.transpose()
+            
+        # print(stores_df)
+        return stores_df
+
+    def extract_from_s3(self, s3_address):
+        """
+        uses the boto3 package to download and extract the information
+        returns: a pandas dataframe
+
+        s3 address: s3://data-handling-public/products.csv
+        """
+        split_url = s3_address.split('/')
+        print(split_url)
+        s3.download_file(f'{split_url[2]}', f'{split_url[3]}', f'local_{split_url[3]}')
+        products = pd.read_csv(f'local_{split_url[3]}')
+        # print(products.head(10))
+        return products
+        
+
 
 if __name__ == "__main__":
     datex = DataExtractor()
     # datex.read_rds_table()
-    datex.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+    # datex.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+    # datex.list_number_of_stores('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores')
+
+    # datex.retrieve_stores_data(f'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details', 
+                                # 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores')
+    datex.extract_from_s3('s3://data-handling-public/products.csv')
--- main.py
+++ main.py
@@ -16,26 +16,55 @@ def main():
     # clean data
     cleaner = DataCleaner(user_table=user_table)
     cleaned_df = cleaner.clean_user_data()
-    
+
+    # # connect to local pgadmin db to upload cleaned pandas df to db as SQL table
+    # local_dbcon = DatabaseConnector('local_creds.yaml')
+    # local_dbcon.connect_to_db()
+
+    # # upload cleaned df to local db (pgadmin)
+    # local_dbcon.upload_to_db('dim_users_2', cleaned_df)
+
+    print("END OF MAIN.")
+
+def main_2():
+    card_datex = DataExtractor()
+    card_table = card_datex.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+
+    card_cleaner = DataCleaner(card_table=card_table)
+    cleaned_card_df = card_cleaner.clean_card_data()
+
     # connect to local pgadmin db to upload cleaned pandas df to db as SQL table
     local_dbcon = DatabaseConnector('local_creds.yaml')
     local_dbcon.connect_to_db()
 
     # upload cleaned df to local db (pgadmin)
-    local_dbcon.upload_to_db('dim_users', cleaned_df)
+    local_dbcon.upload_to_db('dim_card_details', cleaned_card_df)
 
-    print("END OF MAIN.")
+def main_3():
+    store_datex = DataExtractor()
+    store_table = store_datex.retrieve_stores_data('https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details', 
+                                                    'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores')
+                                            
+    store_cleaner = DataCleaner(store_table=store_table)
+    cleaned_store_df = store_cleaner.clean_store_data()
+    # print(cleaned_store_df)
 
-def main_2():
-    card_datex = DataExtractor
-    card_table = card_datex.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+    local_dbcon = DatabaseConnector('local_creds.yaml')
+    local_dbcon.connect_to_db()
 
-    card_cleaner = DataCleaner(card_table=card_table)
-    card_cleaner.clean_card_data() #cleaned_card_df = 
+    local_dbcon.upload_to_db('dim_store_details', cleaned_store_df)
 
+def main_4():
+    products_datex = DataExtractor()
+    products_table = products_datex.extract_from_s3('s3://data-handling-public/products.csv')
 
+    products_cleaner = DataCleaner(products_table=products_table)
+    cleaned_products_df = products_cleaner.clean_products_data()
+    print(cleaned_products_df)
 
 
 if __name__ == "__main__":
     # main()
-    main_2()
+    # main_2()
+    # main_3()
+    main_4()
