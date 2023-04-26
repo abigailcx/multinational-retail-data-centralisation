07e6add9fb3149f81de7a9bdcb2ed89c382aa317
Abigail Cox <abigailgcox@outlook.com>
2023-04-26 20:08:59 +0100

    Adds tidier code

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,10 +1,5 @@
-from dateutil import parser
-import datetime
 import numpy as np
 import pandas as pd
-import sqlalchemy
-import re
-from tqdm import tqdm
 
 class DataCleaner:
     """
@@ -24,16 +19,16 @@ class DataCleaner:
         print("Removing null values...")
 
         dataframe.replace('NULL', None, inplace=True)
-        dataframe.replace('', np.nan, inplace=True)
-        dataframe.dropna(inplace=True)
+        dataframe.replace('', None, inplace=True)
+        dataframe.dropna(inplace=True, how='all')
 
 
     @staticmethod
     def remove_nonsense_codes(df):
         print("Removing nonsense codes...")
         # df = df[~df[column_to_normalise].str.contains(r'[A-Z0-9]{8,10}', regex=True)]
-        df.replace(r'^[A-Z0-9]{8,10}$', np.nan, regex=True, inplace=True)
-        df.dropna(inplace=True)
+        df.replace(r'^[A-Z0-9]{8,10}$', None, regex=True, inplace=True)
+        df.dropna(inplace=True, how='all')
 
     @staticmethod
     def normalise_phone_numbers(dataframe, columns_to_normalise):
@@ -49,6 +44,7 @@ class DataCleaner:
     @staticmethod
     def normalise_dates(dataframe, columns_to_normalise):
         """
+        Normalises dates by transforming them into a uniform format YYYY-MM-DD
         issues with date of birth are:
         cell is empty
         / are used instead of - , e.g. 1944/11/30
@@ -60,14 +56,11 @@ class DataCleaner:
             # normalise dates to same format YYYY-MM-DD and NaN for any that can't be converted
             dataframe[col_name] = pd.to_datetime(dataframe[col_name], format='%Y-%m-%d', errors='coerce')
             # drop rows that include NaNs, editing the original dataframe (returns None)
-            dataframe.dropna(inplace=True)
+            # dataframe.dropna(inplace=True, how='all')
  
 
     @staticmethod
     def normalise_month_year_dates(dataframe, columns_to_normalise):
-        """
-
-        """
         # iterates over list of columns (max is currently 2), so not too much iteration
         print("Normalising card expiry dates...")
         for col_name in columns_to_normalise:
@@ -81,6 +74,7 @@ class DataCleaner:
     @staticmethod
     def normalise_email_addresses(dataframe, columns_to_normalise):
         """
+        Normalises email address by verifying that they have the correct syntax. If they contain multiple @ symbols, this is corrected.
         All invalid email addresses include 2 sequential @ symbols
         check if re.fullmatch is true
         if not, str.replace @{2} with single @ 
@@ -99,14 +93,17 @@ class DataCleaner:
     @staticmethod
     def normalise_country_code(dataframe, column_to_normalise):
         """
+        Normalises country code by replacing known errors with corrections and otherwise looking for a regex match of 2 capital letters
         Infers code "GGB" is actually intended to be "GB" (cross-referenced with country column)
         """
         print("Normalising country codes...")
         replacements_dict = {'GGB': 'GB'}
         for existing, replacement in replacements_dict.items():
             dataframe[column_to_normalise] = dataframe[column_to_normalise].str.replace(existing, replacement, regex=False)
+        # dataframe[column_to_normalise] = dataframe[column_to_normalise].str.replace(r'^([A-Z]{2})', replacement, regex=False)
             mask = dataframe[column_to_normalise].str.fullmatch(r'[A-Z]{2}')
-            dataframe = dataframe.loc[mask]
+            print(mask)
+            # dataframe = dataframe.loc[mask]
 
 
     @staticmethod
@@ -128,7 +125,7 @@ class DataCleaner:
     @staticmethod
     def convert_product_weights(df, column_to_normalise):
         """
-
+        Converts product weights by stripping out all measurement letter indicators and converting all weights to kilograms
         """
         print("Converting product weights...")
         df[column_to_normalise] = df[column_to_normalise].str.strip("., ")
@@ -136,7 +133,7 @@ class DataCleaner:
         df[column_to_normalise] = df[column_to_normalise].str.replace(r'^[0-9]+oz$', '', regex=True)
         df[column_to_normalise] = df[column_to_normalise].str.replace(r'^[0-9]+\sx\s[0-9]+$', '', regex=True)
         df.replace('', np.nan, inplace=True)
-        df.dropna(inplace=True)
+        df.dropna(inplace=True, how='all')
         df[column_to_normalise] = df[column_to_normalise].apply(lambda x: float(x)/1000 if 'k' not in x else x)
         df[column_to_normalise] = df[column_to_normalise].astype(str)
         df[column_to_normalise] = df[column_to_normalise].str.replace('k', '', regex=False)
@@ -155,22 +152,17 @@ class DataCleaner:
     @staticmethod
     def verify_integers(df, column):
         df[column] = pd.to_numeric(df[column], downcast='integer', errors="coerce")
-        df.dropna(inplace=True)
+        df.dropna(inplace=True, how='all')
         print(set(df[column]))
 
         return df
 
 
-
     def clean_user_data(self):
         """
-        which will perform the cleaning of the user data.
-
-        You will need clean the user data, 
-        look out for NULL values, 
-        errors with dates, 
-        incorrectly typed values and 
-        rows filled with the wrong information.
+        Cleans user data
+        Removes null values
+        Normalises phone numbers, country codes, email addresses and dates
         """
         self.remove_null_values(self.users_rds_table)
         self.normalise_phone_numbers(self.users_rds_table, ['phone_number'])
@@ -183,7 +175,7 @@ class DataCleaner:
 
     def clean_card_data(self):
         """
-        clean the data to remove any erroneous values, NULL values or errors with formatting.
+        cleans the data to remove any erroneous values, NULL values or errors with formatting.
         """
         self.remove_null_values(self.card_info_table)
         self.normalise_dates(self.card_info_table, ['date_payment_confirmed'])
@@ -195,36 +187,37 @@ class DataCleaner:
 
     def clean_store_data(self):
         """
-        clean store data 
-        returns pandas dataframe
+        Cleans store data 
+        Returns pandas dataframe
 
-        remove 'lat' column - set is {'2XE1OWOC23', 'NULL', 'LACCWDI0SB', None, 'UXMWDMX1LC', '13KJZ890JH', 'N/A', 'OXVE5QR07O', 'A3O5CBWAMD', 'VKA5I8H32X'}
-        clear to see that all are nonsense codes or 'N/A', 'NULL' string or None type
-        
-        remove null values
-
-        remove nonsense codes - do this via the country_code column because 
-        can then just remove any non-2-letter strings
-
-        opening_date: normalise dates to be in YYYY-MM-DD format
-
-        transform in 'continent':
-        'eeEurope' to 'Europe'
-        'eeAmerica' to 'America'
+        Removes 'lat' column - set is {'2XE1OWOC23', 'NULL', 'LACCWDI0SB', None, 'UXMWDMX1LC', '13KJZ890JH', 'N/A', 'OXVE5QR07O', 'A3O5CBWAMD', 'VKA5I8H32X'}
+        All are nonsense codes or 'N/A', 'NULL' string or None type
+        Removes null values
+        Removes nonsense codes 
+        Normalises country codes to be 2 capital letter codes
+        Normalises dates to be in YYYY-MM-DD format
+        Verifies staff numbers
+        Normalises continent data': 'eeEurope' to 'Europe', 'eeAmerica' to 'America'
         """
         self.store_info_table.drop('lat', axis=1, inplace=True)
         self.remove_null_values(self.store_info_table)
+        self.remove_nonsense_codes(self.store_info_table)
         self.normalise_country_code(self.store_info_table, 'country_code')
         self.normalise_dates(self.store_info_table, ['opening_date'])
         self.normalise_continent_data(self.store_info_table, 'continent')
         self.verify_integers(self.store_info_table, 'staff_numbers')
+        self.remove_null_values(self.store_info_table)
+        print(self.store_info_table.shape[0])
+        print(set(self.store_info_table['country_code']))
+        # print(set(self.store_info_table['store_code']))
 
         return self.store_info_table
 
 
     def clean_products_data(self):
         """
-        
+        Cleans products data by removing null values and nonsense codes
+        Converts product weights and normalises dates by calling static methods in data_cleaning.py
         """
         self.remove_null_values(self.products_info_table)
         self.remove_nonsense_codes(self.products_info_table)
@@ -236,9 +229,9 @@ class DataCleaner:
 
     def clean_orders_table(self):
         """
-        remove columns: first_name, last_name and 1 to have table in correct form before uploading to the database
-        orders data contains column headers which are the same in other tables
-        table will act as source of truth for sales data and will be at centre of start base database schema
+        Removes columns: first_name, last_name and 1 to have table in correct form before uploading to the database
+        Orders data contains column headers which are the same in other tables
+        Table will act as source of truth for sales data and will be at centre of star-based database schema
         """
         self.orders_info_table.drop(['first_name', 'last_name', '1', 'level_0'], axis=1, inplace=True)
         self.remove_null_values(self.orders_info_table) # good check to do, but no null data in this instance
@@ -250,9 +243,9 @@ class DataCleaner:
     def clean_dates_table(self):
         self.remove_null_values(self.dates_info_table)
         self.remove_nonsense_codes(self.dates_info_table)
-        self.join_columns(self.dates_info_table, ['year', 'month', 'day'], 'date')
-        self.normalise_dates(self.dates_info_table, ['date'])
-        self.dates_info_table.drop(['year', 'month', 'day'], axis=1, inplace=True)
+        # self.join_columns(self.dates_info_table, ['year', 'month', 'day'], 'date')
+        # self.normalise_dates(self.dates_info_table, ['date'])
+        # self.dates_info_table.drop(['year', 'month', 'day'], axis=1, inplace=True)
         
         return self.dates_info_table
 
--- database_utils.py
+++ database_utils.py
@@ -4,6 +4,7 @@ import yaml
 from sqlalchemy import create_engine
 from sqlalchemy import inspect
 
+
 class DatabaseConnector:
     """
     A class containing methods to connect to the database
@@ -13,6 +14,7 @@ class DatabaseConnector:
         self.creds_filename = creds_filename
         self.creds_dict = {}
 
+
     def read_db_creds(self):
         """
         A method to read 'db_creds.yaml' 
@@ -20,21 +22,15 @@ class DatabaseConnector:
         """
         with open(self.creds_filename, 'r') as f:
             self.creds_dict = yaml.safe_load(f)
-            # print(self.creds_dict)
             
             return self.creds_dict
 
+
     def init_db_engine(self, database_type="postgresql", database_api="psycopg2"):
         """
-        will read the credentials from 
-        the return of read_db_creds and 
-        initialise and return an sqlalchemy database engine object.
+        Initialises database engine
+        Reads the credentials from the return of read_db_creds method and initialise and return an sqlalchemy database engine object.
         """
-        # {'RDS_HOST': 'data-handling-project-readonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com', 
-        # 'RDS_PASSWORD': 'AiCore2022', 
-        # 'RDS_USER': 'aicore_admin', 
-        # 'RDS_DATABASE': 'postgres', 
-        # 'RDS_PORT': 5432}
         url = f"{database_type}+{database_api}://{self.creds_dict['RDS_USER']}:{self.creds_dict['RDS_PASSWORD']}@{self.creds_dict['RDS_HOST']}:{self.creds_dict['RDS_PORT']}/{self.creds_dict['RDS_DATABASE']}"
         # print(url)
         engine = create_engine(url)
@@ -42,10 +38,11 @@ class DatabaseConnector:
 
         return engine
 
+
     def list_db_tables(self):
         """
-        list all the tables in the database so you know which tables you can extract data from.
-        Use your list_db_tables method to get the name of the table containing user data.
+        Lists all the tables in the database to work out what table to extract data from.
+        Returns: the name of the table containing user data.
         """
         engine = self.init_db_engine()
         inspector = inspect(engine)
@@ -53,19 +50,16 @@ class DatabaseConnector:
     
         return inspector.get_table_names()
 
-        # engine.execute()
 
     def connect_to_db(self):
         self.read_db_creds()
         self.init_db_engine()
         self.list_db_tables()
-        # self.upload_to_db()
+
 
     def upload_to_db(self, table_name, dataframe):
         """
-        This method takes in a Pandas DataFrame and table name to upload to as an argument.
-        
-        Use this method to store the data in your Sales_Data database in a table named dim_users
+        Input: table name to use in postgres Sales_Data database, Pandas data frame to be uploaded
         """
         dataframe.to_sql(table_name, self.init_db_engine())
 
@@ -73,11 +67,3 @@ class DatabaseConnector:
 if __name__ == "__main__":
     dbcon = DatabaseConnector('db_creds.yaml')
     dbcon.connect_to_db()
-
-    #local_dbcon = DatabaseConnector('local_creds.yaml')
-    # local_dbcon.run()
-    #print(local_dbcon.list_db_tables())
-    #test_table = DataExtractor.read_rds_table(local_dbcon.init_db_engine(), 'legacy_users' ) # local_dbcon.list_db_tables()[1]
-    # dc = DataCleaner()
-
-    #local_dbcon.upload_to_db('dim_users', test_table)
